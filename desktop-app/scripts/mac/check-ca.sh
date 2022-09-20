87ad8552a08034d98ffb91dc0f87fe33dfe0c616
liamzebedee <liamzebedee@yahoo.com.au>
2022-09-20 17:03:03 +1000

    cleanup

--- desktop-app/scripts/mac/check-ca.sh
+++ desktop-app/scripts/mac/check-ca.sh
@@ -1,6 +1,5 @@
 set -ex
-# Add the Dappnet root CA to the system Keychain for macOS.
-# This CA signs all *.eth domains.
+
 export CERT=./node_modules/@dappnet/local-gateway/dev-certificates/ca.crt
 
 # This is verified to work on macOS 12.5.1 (21G83).
