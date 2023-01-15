d657043e3ebda4c9480f208b6e0ce760cf8c9f00
liamzebedee <liamzebedee@yahoo.com.au>
2023-01-15 23:48:46 +1100

    .

--- verifiable-rpc/README.md
+++ verifiable-rpc/README.md
@@ -5,3 +5,12 @@ This implements verifiable RPC for Ethereum dapp developers.
 
 This library runs a local EVM, which loads all contract code and storage dynamically while executing a transaction. It connects to a remote RPC node like Infura, and **verifiably** loads all state using storage proofs and `eth_getProof`.
 
+## Demo.
+
+```sh
+DEBUG=ethjs,evm:*:*,evm:*,evm npm run start
+```
+
+```js
+
+```
--- verifiable-rpc/src/index.ts
+++ verifiable-rpc/src/index.ts
@@ -1,18 +1,13 @@
 import * as ethers from 'ethers'
-
 import { Blockchain } from '@ethereumjs/blockchain'
 import { Chain, Common, Hardfork } from '@ethereumjs/common'
 import { EEI } from '@ethereumjs/vm'
 import { EVM } from '@ethereumjs/evm'
 import { EVMOpts } from '@ethereumjs/evm/src/evm'
 import { DefaultStateManager } from '@ethereumjs/statemanager'
-
 import type { StateManager } from '@ethereumjs/statemanager'
 import { Address } from '@ethereumjs/util'
 import { Account } from '@ethereumjs/util'
-
-import { keccak256 } from 'ethereum-cryptography/keccak'
-
 
 const provider = new ethers.providers.InfuraProvider()
 // const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
@@ -154,6 +149,9 @@ class VerifiableEthExecEnv extends EEI implements HookedStateAccess {
     // EEI.
     // 
 
+    // None of these are actually used AFAICS.
+    // Which is why they weren't fixed to use the `this._getProof` API.
+
     /**
      * Returns balance of the given account.
      * @param address - Address of account
@@ -270,22 +268,82 @@ async function main() {
     //
     // (base) ➜  verifiable-rpc git:(master) ✗ cast abi-encode "contenthash(bytes32 node)(bytes memory)" $(cast --from-ascii "tornadocash.eth")
     // 0x746f726e61646f636173682e6574680000000000000000000000000000000000
-    const iface = new ethers.utils.Interface([
-        "function addr(bytes32 node) public view returns (address)",
-        "function contenthash(bytes32 node) external view returns (bytes memory)"
-    ])
-    const CONTRACT_ADDRESS = '0xd3ddccdd3b25a8a7423b5bee360a42146eb4baf3'
-    const calldata = iface.encodeFunctionData("contenthash", ["0xe6ae31d630cc7a8279c0f1c7cbe6e7064814c47d1785fa2703d9ae511ee2be0c"])
-    
-    
-    // const calldata = iface.encodeFunctionData("contenthash", [ethers.utils.formatBytes32String("vitalik.eth")])
+    const encodeCall = (address: string, sig: string, params: any[]) => {
+        const iface = new ethers.utils.Interface([sig])
+        const calldata = iface.encodeFunctionData(
+            sig.split('(')[0].replace('function ', ''), 
+            params
+        )
+        const msg = {
+            from: ethers.constants.AddressZero,
+            to: address,
+            data: calldata,
+        }
+
+        // Encode for evm lib.
+        const msgCoded = {
+            to: Address.fromString(msg.to),
+            caller: Address.fromString(msg.from),
+            data: Buffer.from(msg.data.slice(2), 'hex'),
+
+            // Gas.
+            gasLimit: ethers.constants.MaxUint256.toBigInt(),
+            gasRefund: ethers.constants.MaxUint256.toBigInt(),
+            gasPrice: BigInt(0x1),
+            skipBalance: true,
+        }
+
+        return msgCoded
+    }
 
     // const iface = new ethers.utils.Interface([
-    //     "function balanceOf(address) public view returns (uint256)"
+    //     "function addr(bytes32 node) public view returns (address)",
+    //     "function contenthash(bytes32 node) external view returns (bytes memory)"
     // ])
-    // const CONTRACT_ADDRESS = '0x6b175474e89094c44da98b954eedeac495271d0f'
-    // // const calldata = iface.encodeFunctionData("contenthash", [ethers.utils.formatBytes32String("vitalik.eth")])
-    // const calldata = iface.encodeFunctionData("balanceOf", ["0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"])
+    // const CONTRACT_ADDRESS = '0xd3ddccdd3b25a8a7423b5bee360a42146eb4baf3'
+    // const calldata = iface.encodeFunctionData("contenthash", ["0xe6ae31d630cc7a8279c0f1c7cbe6e7064814c47d1785fa2703d9ae511ee2be0c"])
+
+    // Get vitalik.eth's DAI balance.
+    const msg = encodeCall(
+        "0x6b175474e89094c44da98b954eedeac495271d0f",
+        "function balanceOf(address) public view returns (uint256)",
+        ["0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"]
+    )
+    
+    // Lookup tornadocash.eth
+    // const msg = encodeCall(
+    //     "0xd3ddccdd3b25a8a7423b5bee360a42146eb4baf3", 
+    //     "function contenthash(bytes32 node) external view returns (bytes memory)", 
+    //     ["0xe6ae31d630cc7a8279c0f1c7cbe6e7064814c47d1785fa2703d9ae511ee2be0c"]
+    // )
+    
+    
+
+    // const solc = require('solc');
+
+    // const input = {
+    //     language: 'Solidity',
+    //     sources: {
+    //         'test.sol': {
+    //             content: 'contract C { function f() public { } }'
+    //         }
+    //     },
+    //     settings: {
+    //         outputSelection: {
+    //             '*': {
+    //                 '*': ['*']
+    //             }
+    //         }
+    //     }
+    // };
+
+    // const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+
+
+    // const calldata = iface.encodeFunctionData("contenthash", [ethers.utils.formatBytes32String("vitalik.eth")])
+
+    
 
     // const iface = new ethers.utils.Interface([
     //     "function getCount(uint256) public view returns (uint256)"
@@ -294,12 +352,12 @@ async function main() {
     // // const calldata = iface.encodeFunctionData("contenthash", [ethers.utils.formatBytes32String("vitalik.eth")])
     // const calldata = iface.encodeFunctionData("getCount", ["123"])
 
+    // const msg = {
+    //     from: ethers.constants.AddressZero,
+    //     to: CONTRACT_ADDRESS,
+    //     data: calldata,
+    // }
     
-    const msg = {
-        from: ethers.constants.AddressZero,
-        to: CONTRACT_ADDRESS,
-        data: calldata,
-    }
 
     // console.log(msg)
 
@@ -316,22 +374,10 @@ async function main() {
     // })
 
 
-    // Now load the code.
-    const msgCoded = {
-        to: Address.fromString(msg.to),
-        caller: Address.fromString(msg.from),
-        data: Buffer.from(msg.data.slice(2), 'hex'),
-        
-        // Gas.
-        gasLimit: ethers.constants.MaxUint256.toBigInt(),
-        gasRefund: ethers.constants.MaxUint256.toBigInt(),
-        gasPrice: BigInt(0x1),
-        skipBalance: true,
-    }
     // console.log(msgCoded)
     
     try {
-        const { execResult: res } = await evm.runCall(msgCoded)
+        const { execResult: res } = await evm.runCall(msg)
         console.log(res.exceptionError)
         console.log(`Returned: ${res.returnValue.toString('hex')}`)
         console.log(`gasUsed: ${res.executionGasUsed.toString()}`)
