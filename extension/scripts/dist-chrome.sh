fd60159f01cca616b21a15148c80c932c820b403
liamzebedee <liamzebedee@yahoo.com.au>
2023-04-03 11:55:07 +1000

    submit to chrome webstore

--- extension/scripts/dist-chrome.sh
+++ extension/scripts/dist-chrome.sh
@@ -5,13 +5,15 @@ set -ex
 PRODUCT=dappnet-extension
 BASE=$(pwd)
 
-# Bump version.
-cd $BASE/src/chrome
-MANIFEST_VERSION=$(cat manifest.json | jq -r .version)
-MANIFEST_VERSION_BUMP=$MANIFEST_VERSION
-# MANIFEST_VERSION_BUMP=$(node -e "let v = process.argv[1].split('.'); console.log(v[0] + '.' + v[1] + '.' + (parseInt(v[2]) + 1))" $MANIFEST_VERSION)
-NEW_MANIFEST=$(jq --arg newVersion "$MANIFEST_VERSION_BUMP" '.version = $newVersion' manifest.json)
-echo "$NEW_MANIFEST" > manifest.json
+# Check if .zip already exists.
+cd $BASE/dist/
+VERSION=$(cat $BASE/src/chrome/manifest.json | jq -r .version)
+BROWSER=chrome
+RELEASE=$PRODUCT\_$BROWSER\_$VERSION
+if [ -f "$RELEASE.zip" ]; then
+  echo "$RELEASE.zip already exists."
+  exit 1
+fi
 
 # Rebuild.
 cd $BASE
